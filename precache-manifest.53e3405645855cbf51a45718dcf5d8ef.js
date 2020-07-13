4ec6faaab80fef012ebad8ad7cbaf2b9fa0e1e19
Helder S�rvio <heldersrvio@gmail.com>
2020-07-12 21:38:58 -0300

    Updates

---
+++ precache-manifest.53e3405645855cbf51a45718dcf5d8ef.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e84aa4680844645430f94fb2b60b7eb",
+    "url": "/Memory-Card-Game/index.html"
+  },
+  {
+    "revision": "c3e93d6649f71bacccf4",
+    "url": "/Memory-Card-Game/static/css/main.39dcf15d.chunk.css"
+  },
+  {
+    "revision": "473f581b3ea302a17982",
+    "url": "/Memory-Card-Game/static/js/2.7da10074.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Memory-Card-Game/static/js/2.7da10074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3e93d6649f71bacccf4",
+    "url": "/Memory-Card-Game/static/js/main.44b7cc1b.chunk.js"
+  },
+  {
+    "revision": "d6b72fc4b6011fe7177e",
+    "url": "/Memory-Card-Game/static/js/runtime-main.d0e656c9.js"
+  },
+  {
+    "revision": "d9d14c90e12350dea17ef25bb7b50c0f",
+    "url": "/Memory-Card-Game/static/media/anne.d9d14c90.jpg"
+  },
+  {
+    "revision": "824ac0d70b91ba0b9fa2a9c6bb0f4b5b",
+    "url": "/Memory-Card-Game/static/media/croaker.824ac0d7.png"
+  },
+  {
+    "revision": "6e597003b10ddd0c1810593621c6417d",
+    "url": "/Memory-Card-Game/static/media/grime.6e597003.png"
+  },
+  {
+    "revision": "5badcd86b7a719634df92ebfe42c447e",
+    "url": "/Memory-Card-Game/static/media/hoppop.5badcd86.png"
+  },
+  {
+    "revision": "c5790b4e41da9a57adf831e6c0818552",
+    "url": "/Memory-Card-Game/static/media/loggle.c5790b4e.png"
+  },
+  {
+    "revision": "794635a69f668aa0587ed2a8efd1af76",
+    "url": "/Memory-Card-Game/static/media/maddie.794635a6.jpg"
+  },
+  {
+    "revision": "7702daf7b4d21e13ac9b52a71ea417ba",
+    "url": "/Memory-Card-Game/static/media/polly.7702daf7.png"
+  },
+  {
+    "revision": "5e94ec834458a9fa452796f1f1f55e7b",
+    "url": "/Memory-Card-Game/static/media/sasha.5e94ec83.jpg"
+  },
+  {
+    "revision": "57a1fbf31ec5ab303833c0d2288bb6f9",
+    "url": "/Memory-Card-Game/static/media/sprig.57a1fbf3.png"
+  },
+  {
+    "revision": "fc5ee06d667e1354b7ed052075039930",
+    "url": "/Memory-Card-Game/static/media/sylvia.fc5ee06d.png"
+  },
+  {
+    "revision": "2b499bfef2a0dd6a07bb9d0cfd274032",
+    "url": "/Memory-Card-Game/static/media/toadstool.2b499bfe.jpg"
+  },
+  {
+    "revision": "8036609c2caa19ea249a68ebab69c2d0",
+    "url": "/Memory-Card-Game/static/media/wally.8036609c.png"
+  }
+]);
